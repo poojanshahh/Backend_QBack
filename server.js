4597a162c5c4f20fd9ea8f77103afdb5d3c0d454
Poojan Shah <poojanshah1812@gmail.com>
2023-12-23 13:34:08 +0530

    update & delete APIs

--- server.js
+++ server.js
@@ -54,6 +54,60 @@ app.get('/api/users', (req, res) => {
     });
 });
 
+app.get('/api/users/:userId', (req, res) => {
+    const { userId } = req.params;
+    User.findById(userId, (err, user) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error fetching user');
+        } else if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+        });
+    });
+
+    app.put('/api/users/update/:username', (req, res) => {
+        const { username } = req.params;
+        const updatedUserData = req.body;
+    
+        User.findOneAndUpdate(
+        { username: username },
+        updatedUserData,
+        { new: true },
+        (err, updatedUser) => {
+            if (err) {
+            console.error(err);
+            res.status(500).send('Error updating user');
+            } else if (updatedUser) {
+            res.status(200).json(updatedUser);
+            } else {
+            res.status(404).json({ error: 'User not found' });
+            }
+        }
+        );
+    });
+    
+    
+    app.delete("/api/users/:userId", async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+        const deletedUser = await User.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        res.json({ message: "User deleted successfully", deletedUser });
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ error: "Failed to delete user" });
+    }
+});
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
